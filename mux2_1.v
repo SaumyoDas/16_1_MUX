9df8d59cfc3aeabcb51615a1e774ee1b5643914f
SaumyoDas <164749799+SaumyoDas@users.noreply.github.com>
2024-08-30 12:32:10 +0530

    Update mux2_1.v

--- mux2_1.v
+++ mux2_1.v
@@ -3,8 +3,8 @@ input [1:0] i;
 input s0;
 output y;
 wire t1,t2,t3;
-NOT G1(t1,s0);
-AND G2(t2,i[0],t1);
-AND G3(t3,i[1],s0);
-OR G4(y,t2,t3);
+not G1(t1,s0);
+and G2(t2,i[0],t1);
+and G3(t3,i[1],s0);
+or G4(y,t2,t3);
 endmodule
